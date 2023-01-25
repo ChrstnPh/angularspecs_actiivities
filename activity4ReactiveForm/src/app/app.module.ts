35ace87a30b8b788a6ad9d473742fd76231a1a79
Kwin <106297622+ChrstnPh@users.noreply.github.com>
2023-01-25 14:07:43 +0800

    Update app.module.ts

--- activity4ReactiveForm/src/app/app.module.ts
+++ activity4ReactiveForm/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import{ HttpClientModule} from'@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { AddTaskComponent } from './add-task/add-task.component';
@@ -13,7 +15,8 @@ import { AddTaskComponent } from './add-task/add-task.component';
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
